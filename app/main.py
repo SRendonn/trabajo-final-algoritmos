99731521183e9c746d8c97ab734bfa9b4865a714
Juan Pablo Ortega Medina <jportegame@unal.edu.co>
2022-02-09 04:31:45 -0500

    asignar domiciliarios

--- app/main.py
+++ app/main.py
@@ -21,8 +21,8 @@ def graficar(punto: Punto, color):
 
 def main():
 
-    numero_domiciliarios: int = 3
-    numero_puntos_entrega: int = 8
+    numero_domiciliarios: int = 10
+    numero_puntos_entrega: int = 100
     generador: Generador = Generador(numero_domiciliarios, numero_puntos_entrega)
 
     datos: Dict[str, Any] = generador.run()
@@ -77,6 +77,7 @@ def main():
     agglomerative_clustering = AgglomerativeClustering(domiciliarios, puntos_entrega)
 
     clusters = agglomerative_clustering.run()
+        
 
     plt.xlim(0, 11)
     plt.ylim(0, 11)
@@ -95,6 +96,21 @@ def main():
     for punto in domiciliarios:
        coordenadas = punto.get()
        plt.plot(coordenadas[0], coordenadas[1], marker="X", color="black")
+
+    domiciliarios_por_asignar = domiciliarios[:]
+
+    while len(domiciliarios_por_asignar) > 0:
+        distancias = [[x, i, Punto.get_punto_mas_cercano(x, clusters)] for i,x in enumerate(domiciliarios_por_asignar)]
+        maximo_minimo = max(distancias, key=lambda x: x[2][1])
+
+        x_values = [maximo_minimo[0].get_x(), maximo_minimo[2][0].get_x()]
+        y_values = [maximo_minimo[0].get_y(), maximo_minimo[2][0].get_y()]
+        plt.plot(x_values, y_values, color="black")
+
+        plt.plot(maximo_minimo[2][0].get_x(), maximo_minimo[2][0].get_y(), marker="*", color='black')
+
+        del domiciliarios_por_asignar[maximo_minimo[1]]
+        del clusters[maximo_minimo[2][0].get_index()]
 
     plt.show()
 
